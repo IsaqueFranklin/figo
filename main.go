7190542b85c141e06d4e3f6f8e033c447aa37506
Isaque Franklin Amaral <isaquefrankli@gmail.com>
2024-03-27 21:33:44 -0300

    counter func

--- main.go
+++ main.go
@@ -2,14 +2,19 @@ package main
 
 import (
   //"encoding/json"
-  //"fmt"
+  "fmt"
   //"io/ioutil"
   "log"
   //"net/http"
-  //"time"
+  "time"
   "github.com/gofiber/template/html/v2"
   "github.com/gofiber/fiber/v2"
+  "strconv"
 )
+
+type Count struct {
+  Number int
+}
 
 func main() {
 
@@ -22,7 +27,30 @@ func main() {
   }) 
 
   app.Get("/", func(ctx *fiber.Ctx) error {
-    return ctx.Render("index", fiber.Map{})
+    return ctx.Render("index", fiber.Map{
+      "Count": 0,
+    })
+  })
+  
+  app.Post("/count", func(ctx *fiber.Ctx) error {
+
+    time.Sleep(1 *time.Second)
+    count := ctx.FormValue("count")
+   
+    fmt.Println(count)
+    
+    num, err := strconv.Atoi(count)
+	  if err != nil {
+		  fmt.Println("Conversion failed:", err)
+		  return nil
+	  }
+	  fmt.Println("Converted integer:", num)
+    
+    numer := num + 1
+
+    return ctx.Render("index", fiber.Map{
+      "Count": numer,
+    })
   })
 
   log.Fatal(app.Listen(":9000"))
